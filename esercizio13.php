36225beea83eb74c9be79d1c6e8acf8f281153d8
Benncio09 <lorebencini13@outlook.com>
2024-10-03 09:08:29 +0200

    finito e funzionante

--- esercizio13.php
+++ esercizio13.php
@@ -6,6 +6,15 @@
     <title>Document</title>
 </head>
 <body>
+
+    <?php
+        function generaUrlImmagine($larghezza, $altezza){
+            return "https://picsum.photos/$larghezza/$altezza";
+
+        }
+        $url=generaUrlImmagine(600,300);
+        echo "<img src='$url'>";
+    ?>
     
 </body>
 </html>
